630599a785af86d70674c8a8e24df305b60c3317
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2026-07-19 18:57:06 +0000

    Cleanup

--- filesystem-context/sample_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def list_files(directory):
-    """Lists all files in a directory."""
-    return os.listdir(directory)
-
-def get_file_size(filepath):
-    """Returns the size of a file in bytes."""
-    return os.path.getsize(filepath)
